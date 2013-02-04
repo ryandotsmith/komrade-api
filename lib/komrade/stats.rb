4288918b77706956e0ce172f4dc2b79f1048c80d
♠ ace hacker <ryan@heroku.com>
2013-02-03 19:39:20 -0800

    show lost jobs

--- lib/komrade/stats.rb
+++ lib/komrade/stats.rb
@@ -7,7 +7,12 @@ module Komrade
     DELETE = 2
 
     def all(queue_id)
-      {in: _in(queue_id), out: out(queue_id), error: error(queue_id)}
+      {
+        in: _in(queue_id),
+        out: out(queue_id),
+        error: error(queue_id),
+        lost_jobs(queue_id)
+      }
     end
 
     def _in(queue_id)
@@ -26,12 +31,29 @@ module Komrade
       }
     end
 
+    def lost(queue_id)
+      {
+        minute: lost_jobs(queue_id, 'minute'),
+        hour: lost_jobs(queue_id, 'hour'),
+        day: lost_jobs(queue_id, 'day')
+      }
+    end
+
     def error(queue_id)
       {
         minute: failed_jobs(queue_id, 'minute'),
         hour: failed_jobs(queue_id, 'hour'),
         day: failed_jobs(queue_id, 'day')
       }
+    end
+
+    def lost_jobs(qid, period)
+      pg[:jobs].
+        where(queue: qid).
+        where("locked_at is not null").
+        where("failed_at = 0").
+        where("heartbeat - now() < '1 #{period}'::interval").
+        count
     end
 
     def failed_jobs(qid, period)
