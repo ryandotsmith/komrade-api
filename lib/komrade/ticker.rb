0c2e5253018684cb7a5fb119de51884773710c5c
♠ ace hacker <ryan@heroku.com>
2013-03-01 15:08:38 -0800

    compact hourly stats

--- lib/komrade/ticker.rb
+++ lib/komrade/ticker.rb
@@ -9,6 +9,7 @@ module KomradeApi
       loop do
         t = Time.now
         process_minute(t) if t.sec % 30 == 0
+        process_hour(t) if t.min % 30 == 0
         sleep(1)
       end
     end
@@ -17,6 +18,13 @@ module KomradeApi
       puts("at=process-minute")
       Queue.all.each do |queue|
         Komrade::Queue.enqueue("KomradeApi::StatsMin.compact", queue[:token], t.to_i)
+      end
+    end
+
+    def process_hour(t)
+      puts("at=process-minute")
+      Queue.all.each do |queue|
+        Komrade::Queue.enqueue("KomradeApi::StatsHour.compact", queue[:token], t.to_i)
       end
     end
 
