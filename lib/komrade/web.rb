06ab2beb2ca9f2b20f62c4485851af1f45713858
♠ ace hacker <ryan@heroku.com>
2013-03-02 23:34:35 -0800

    look at a 60s moving window

--- lib/komrade/web.rb
+++ lib/komrade/web.rb
@@ -93,7 +93,7 @@ module KomradeApi
       when 'day'
         StatsHour.aggregate(@queue[:token])
       else
-        StatsRaw.aggregate(@queue[:token], Time.now - 3)
+        StatsRaw.aggregate(@queue[:token], Time.now - 60)
       end
       status(200)
       body(JSON.dump(res))
