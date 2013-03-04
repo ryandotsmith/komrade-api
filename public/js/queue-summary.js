938e4f834cab11cc4db9c5b84aedbc39bcaa57e4
♠ ace hacker <ryan@heroku.com>
2013-03-03 18:21:11 -0800

    compact

--- public/js/queue-summary.js
+++ public/js/queue-summary.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
 
 function updateSummary() {
 	var an = _.template("<span>App: <strong><%=name%></strong></span>");
-	var ql = _.template("<span>Queue Length: <strong><%=length%></strong></span>");
+	var ql = _.template("<span>QueueLength: <strong><%=length%></strong></span>");
 
 	$.getJSON("/summary", function(data) {
 		$("#queue-summary").children().remove();
